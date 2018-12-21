bfd01cc2fae59e2bbb0beb09906d1a2a765a62ce
Radoslaw Tereszczuk <tereszczuk@gmail.com>
2018-12-21 19:27:03 +0100

    ff

--- m0/m0Main.xaml.cs
+++ m0/m0Main.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
 
 using m0.UIWpf;
 using m0.Graph;
@@ -54,9 +56,105 @@ namespace m0
             GraphUtil.ReplaceEdge(stv.Vertex.Get("BaseEdge:"), "To", MinusZero.Instance.Root);                                    
             
             this.root.Content=stv;
+
+
+            this.Loaded += new RoutedEventHandler(m0Main_Loaded);
+
+            this_static = this;
+
         }
 
-        
+
+        // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+        // SYSTEM MENU BEG
+
+        // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+        #region Win32 API Stuff
+
+        // Define the Win32 API methods we are going to use
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetSystemMenu(IntPtr hWnd, bool bRevert);
+
+        [DllImport("user32.dll")]
+        private static extern bool InsertMenu(IntPtr hMenu, Int32 wPosition, Int32 wFlags, Int32 wIDNewItem, string lpNewItem);
+
+        /// Define our Constants we will use
+        public const Int32 WM_SYSCOMMAND = 0x112;
+        public const Int32 MF_SEPARATOR = 0x800;
+        public const Int32 MF_BYPOSITION = 0x400;
+        public const Int32 MF_STRING = 0x0;
+
+        #endregion
+
+        // The constants we'll use to identify our custom system menu items
+        public const Int32 _TransactionSysMenuID = 1000;
+        public const Int32 _AboutSysMenuID = 1001;
+
+        /// <summary>
+        /// This is the Win32 Interop Handle for this Window
+        /// </summary>
+        public IntPtr Handle
+        {
+            get
+            {
+                return new WindowInteropHelper(this).Handle;
+            }
+        }
+
+        private void m0Main_Loaded(object sender, RoutedEventArgs e)
+        {
+            /// Get the Handle for the Forms System Menu
+            IntPtr systemMenuHandle = GetSystemMenu(this.Handle, false);
+
+            /// Create our new System Menu items just before the Close menu item
+            InsertMenu(systemMenuHandle, 5, MF_BYPOSITION | MF_SEPARATOR, 0, string.Empty); // <-- Add a menu seperator
+            InsertMenu(systemMenuHandle, 6, MF_BYPOSITION, _TransactionSysMenuID, "Transactions");
+            InsertMenu(systemMenuHandle, 7, MF_BYPOSITION, _AboutSysMenuID, "About");
+
+            // Attach our WndProc handler to this Window
+            HwndSource source = HwndSource.FromHwnd(this.Handle);
+            source.AddHook(new HwndSourceHook(WndProc));
+        }
+
+        static m0Main this_static;
+
+        private static IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            // Check if a System Command has been executed
+            if (msg == WM_SYSCOMMAND)
+            {
+                // Execute the appropriate code for the System Menu item that was clicked
+                switch (wParam.ToInt32())
+                {
+                    case _TransactionSysMenuID:
+                        MessageBox.Show("\"Settings\" was clicked");
+                        handled = true;
+                        break;
+                    case _AboutSysMenuID:
+
+                        m0.UIWpf.Forms.About about = new UIWpf.Forms.About();
+
+                        about.Owner = this_static;
+
+                        about.Show();
+
+                        handled = true;
+                        break;
+                }
+            }
+
+            return IntPtr.Zero;
+        }
+
+        // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+        // SYSTEM MENU END
+
+        // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
 
         private string randomChars()
         {
