734f220ecbe9b52392fc9824c7b1f19d67f739d3
rad9k <tereszczuk@gmail.com>
2019-01-19 15:19:52 +0100

    d

--- m0/Store/FileSystem/FileSystemUtil.cs
+++ m0/Store/FileSystem/FileSystemUtil.cs
@@ -8,9 +8,31 @@ namespace m0.Store.FileSystem
 {
     public class FileSystemUtil
     {
+        public static string addNew(string fileName)
+        {
+            
+        }
+
         public static string getExtension(string fileName)
         {
-            return fileName.Substring(fileName.LastIndexOf('.'));
+            int dotpos = fileName.LastIndexOf('.');
+
+            if (dotpos == -1)
+                return "";
+
+            int slashpos = fileName.LastIndexOf('\\');
+
+            if (slashpos == -1)
+                return fileName.Substring(dotpos+1);
+
+            fileName = fileName.Substring(slashpos);
+
+            dotpos = fileName.LastIndexOf('.');
+
+            if (dotpos == -1)
+                return "";
+
+            return fileName.Substring(dotpos);
         }
 
         public static string getFileName(string fileName)
