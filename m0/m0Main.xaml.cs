f50225f567e2e4f9a95e04f034cca445033fd6df
rad9k <tereszczuk@gmail.com>
2019-06-05 23:25:39 +0200

    l

--- m0/m0Main.xaml.cs
+++ m0/m0Main.xaml.cs
@@ -212,7 +212,7 @@ namespace m0
 
             //b.AddEdge(r.Get(false, @"System\Meta*$Inherits"), a);
 
-            IVertex re = ((EasyVertex)bas).NewGetAll(false, @"\");
+            IVertex re = ((EasyVertex)bas).NewGetAll(false, @"ma|a\maa|aa");
             re = ((EasyVertex)bas).NewGetAll(false, @"");
             
         }
