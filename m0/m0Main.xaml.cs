186195a2d9d543951fd3247da0103c635cdcf892
Radoslaw Tereszczuk <tereszczuk@gmail.com>
2019-06-19 22:32:11 +0200

    a

--- m0/m0Main.xaml.cs
+++ m0/m0Main.xaml.cs
@@ -240,10 +240,10 @@ namespace m0
 
             m0.MinusZero.Instance.LogLevel = -2;
 
-            q(bas, @"ma|a\|aa2");
-            q(bas, @"ma\aa2");
-            q(bas, @"ma|a\|aa\");
-            q(bas, @"\\");
+            q(bas, @"{aa}");
+            q(bas, @"{aa2}");
+            q(bas, @"{aa,aa2}");
+            
 
             /*   q(bas, @"");
                q(bas, @"mb|b");
