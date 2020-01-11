587abc0f5bb57607c1e4f27117b026f9e7532739
rad9k <tereszczuk@gmail.com>
2020-01-12 00:05:35 +0100

    forward

--- m0_SYSTEM_GENERATE/Program.cs
+++ m0_SYSTEM_GENERATE/Program.cs
@@ -4,12 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using m0;
+
 namespace m0_SYSTEM_GENERATE
 {
     class Program
     {
         static void Main(string[] args)
         {
+               
         }
     }
 }
