d53b0dba14338199af2dc27a3f39a4bb3ff1294b
rad9k <tereszczuk@gmail.com>
2020-06-08 18:01:15 +0200

    a

---
+++ m0_COMPOSER/UIWpf/Visualisers/IZoomScrollViewerHost.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace m0_COMPOSER.UIWpf.Visualisers
+{
+    public interface IZoomScrollViewerHost
+    {
+        void SetZoomFactors(double horizontalZoomFactor, double verticalZoomFactor);
+    }
+}
