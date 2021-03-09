85f672a0728a5528c96529bb4489b6f8535b71bd
rad9k <tereszczuk@gmail.com>
2021-03-09 23:11:31 +0100

    f

--- m0_composer/uiwpf/visualisers/control/common.cs
+++ m0_composer/uiwpf/visualisers/control/common.cs
@@ -10,6 +10,33 @@ using System.Windows.Shapes;
 
 namespace m0_COMPOSER.UIWpf.Visualisers.Control
 {
+
+    class PrimLines
+    {
+        public Line LineBeg;
+        public Line LineEnd;
+
+        public double LineBegPosition;
+        public double LineEndPosition;
+
+        public PrimLines(Canvas c, double position1, double position2, double height)
+        {         
+            LineBeg = WpfUtil.DrawLine(c, position1, 0, position1, height, 3, (Brush)WpfUtil.FindResource("0HardHighlightPrimBrush"));
+
+            LineEnd = WpfUtil.DrawLine(c, position2, 0, position2, height, 3, (Brush)WpfUtil.FindResource("0HardHighlightPrimBrush"));
+
+            Panel.SetZIndex(LineBeg, 1000);
+
+            Panel.SetZIndex(LineEnd, 1000);            
+        }
+
+        public static void UpdatePositionMarkPrim(PrimLines pl, double position, double height)
+        {
+            WpfUtil.SetLinePosition(l, position, 0, position, height);
+        }
+
+    }
+
     class Common
     {
         public static Line CreatePositionMark(Canvas c, double position, double height)
@@ -19,11 +46,6 @@ namespace m0_COMPOSER.UIWpf.Visualisers.Control
             Panel.SetZIndex(l, 1000);
 
             return l;
-        }
-
-        public static void UpdatePositionMark(Line l, double position, double height)
-        {
-            WpfUtil.SetLinePosition(l, position, 0, position, height);
-        }
+        }        
     }
 }
