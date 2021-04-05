6b0aca4f56c79af494773f6d0ea83aa8bec73253
rad9k <tereszczuk@gmail.com>
2021-04-05 11:32:32 +0200

    a

---
+++ m0_COMPOSER/UIWpf/Visualisers/Control/Item/ItemDictionary.cs
@@ -0,0 +1,20 @@
+﻿using m0.Foundation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace m0_COMPOSER.UIWpf.Visualisers.Control.Item
+{
+    public class ItemDictionary
+    {
+        IDictionary<IVertex, IList<IItem>> dict = new Dictionary<IVertex, IList<IItem>>();
+
+        public void AddItem(IItem item)
+        {
+
+        }
+
+    }
+}
