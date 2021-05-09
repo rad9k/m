6684d8de35bba2e2aa0a6e2bdec051c5db9d330d
rad9k <tereszczuk@gmail.com>
2021-05-10 00:02:42 +0200

    seems to be f working. generate

--- m0/Graph/Internal/OutList.cs
+++ m0/Graph/Internal/OutList.cs
@@ -31,24 +31,22 @@ namespace m0.Graph.Internal
 
         public override void OnAdd(IEdge item)
         {
-            if (item.Meta != null)
-                item.Meta.MetaInEdgesRaw.Add(item);
+            if (!ed.NoInEdgeInOutVertexVertexMode)
+            {
+                if (item.Meta != null)
+                    item.Meta.MetaInEdgesRaw.Add(item);
 
-            if(!ed.NoInEdgeInOutVertexVertexMode &&  item.To!=null)
-                item.To.InEdgesRaw.Add(item);
+                if (item.To != null)
+                    item.To.InEdgesRaw.Add(item);
+            }
 
             ed.v.OutEdgesDictionariesNeedsRebuild = true;
 
-            ed.v.InheritChildsDictionariesNeedsRebuild(false);            
+            ed.v.InheritChildsDictionariesNeedsRebuild(false);
         }
 
         public override void OnRemove(IEdge item)
         {
-            if(item.From.Value is string && ((string)item.From.Value) == "Std")
-            {
-                int x = 0;
-            }
-
             item.RemovedFromFromAndMeta = true;
 
             if(item.Meta != null)
