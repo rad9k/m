1741a7bb7c64ee046076746c0237761cf6a155a0
rad9k <tereszczuk@gmail.com>
2021-06-06 00:14:54 +0200

    f

--- m0/ZeroCode/Helpers/InstructionHelpers.cs
+++ m0/ZeroCode/Helpers/InstructionHelpers.cs
@@ -179,7 +179,8 @@ namespace m0.ZeroCode.Helpers
 
             INoInEdgeInOutVertexVertex stack = inStack;
 
-            foreach (IEdge e in baseVertex)
+            foreach (IEdge e in baseVertex.ToList())
+            //foreach (IEdge e in baseVertex) // XXX
                 //stack = exe.ExecuteInstruction(stack, e.To); // XXX another interesting processing approach
                 if (!ZeroCodeUtil.ShouldNotExecute(e))
                 { // XXX in some cases it might not work - instruction with meta begginning with $ will not be executed. nor its children
