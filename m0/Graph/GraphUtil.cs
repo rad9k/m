a498b89779c7b5c4c857907f70d26748c4004a31
rad9k <tereszczuk@gmail.com>
2022-01-05 00:43:45 +0100

    h

--- m0/Graph/GraphUtil.cs
+++ m0/Graph/GraphUtil.cs
@@ -64,7 +64,8 @@ namespace m0.Graph
 
         public static void Debug(IVertex v, DebugOperationEnum Operation)
         {
-            if (v.Identifier != null 
+            if (v != null
+                && v.Identifier != null 
                 && v.Identifier is long 
                 && DebugVertex.Contains((long)v.Identifier) 
                 && v.Store.Identifier == DebugStore
