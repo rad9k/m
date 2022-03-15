b075155fff6b0b1f1f1c2243a6338f1ebd186748
rad9k <tereszczuk@gmail.com>
2022-03-15 23:16:58 +0100

    f

--- m0_COMPOSER/UIWpf/Visualisers/MelodyFlowVisualiserBase.cs
+++ m0_COMPOSER/UIWpf/Visualisers/MelodyFlowVisualiserBase.cs
@@ -153,9 +153,21 @@ namespace m0_COMPOSER.UIWpf.Visualisers
             item.Update();
         }
 
+        bool DoStepContainQuant(IVertex quantVertex)
+        {
+            if (GraphUtil.ExistQueryOut(quantVertex, Flow.StepToQuantMeta, null))
+                return true;
+
+            return false;
+        }
+
         protected override void AddItemByEdge(IEdge itemEdge, List<IVertex> selectedVertexes)
         {
             IVertex quantVertex = itemEdge.To;
+            return;
+
+            if (!DoStepContainQuant(quantVertex))
+                return;
 
             FrameworkElement newElement;
 
