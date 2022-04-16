75417afee0d6d6938141fa13ea8b155155185983
rad9k <tereszczuk@gmail.com>
2022-04-16 20:39:05 +0200

    temp edges not connected with root

--- m0/m0.cs
+++ m0/m0.cs
@@ -115,7 +115,7 @@ namespace m0
         public IVertex CreateTempVertex()
         {
             IEdge edge = TempStore.Root.AddVertexAndReturnEdge(empty, null);
-          //  TempStore.Root.DeleteEdge(edge);
+            TempStore.Root.DeleteEdge(edge);
             return edge.To;
         }
 
