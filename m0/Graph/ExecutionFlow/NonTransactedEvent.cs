d2b63dcd29824cfd80b2a8ca33fb23c3c7eb111a
rad9k <tereszczuk@gmail.com>
2022-05-08 23:27:09 +0200

    f

--- m0/Graph/ExecutionFlow/NonTransactedEvent.cs
+++ m0/Graph/ExecutionFlow/NonTransactedEvent.cs
@@ -11,7 +11,8 @@ namespace m0.Graph.ExecutionFlow
     {
         static void FireTrigger(IVertex trigger, GraphChangeTransactionAtom gcta)
         {
-
+            IVertex eventVertex = gcta.CreateEventVertex(trigger, null);            
+                
 
         }
 
@@ -41,43 +42,46 @@ namespace m0.Graph.ExecutionFlow
                         if (GraphUtil.ExistQueryOut(trigger.To, "ChangeTypeFilter", "OutputEdgeDisposed"))
                             FireTrigger(trigger.To, gcta);
                         break;
-                }
-            }                            
+                }                                        
         }
 
         public static void HandleInEdge(GraphChangeTransactionAtom gcta)
         {
-        IVertex changedVeretx = gcta.ChangedVertex;
+            IVertex changedVeretx = gcta.ChangedVertex;
 
-        foreach (IEdge trigger in GraphUtil.GetQueryOut(changedVeretx, "$GraphChangeTrigger", null))
-            switch (gcta.Type)
-            {
-                case AtomGraphChangeTypeEnum.ValueChange:
-                    if (GraphUtil.ExistQueryOut(trigger.To, "ChangeTypeFilter", "ValueChange"))
-                        FireTrigger(trigger.To, gcta);
-                    break;
+            foreach (IEdge trigger in GraphUtil.GetQueryOut(changedVeretx, "$GraphChangeTrigger", null))
+                switch (gcta.Type)
+                {                    
+                    case AtomGraphChangeTypeEnum.EdgeAdded:
+                        if (GraphUtil.ExistQueryOut(trigger.To, "ChangeTypeFilter", "InputEdgeAdded"))
+                            FireTrigger(trigger.To, gcta);
+                        break;
 
-                case AtomGraphChangeTypeEnum.EdgeAdded:
-                    if (GraphUtil.ExistQueryOut(trigger.To, "ChangeTypeFilter", "OutputEdgeAdded"))
-                        FireTrigger(trigger.To, gcta);
-                    break;
+                    case AtomGraphChangeTypeEnum.EdgeRemoved:
+                        if (GraphUtil.ExistQueryOut(trigger.To, "ChangeTypeFilter", "InputEdgeRemoved"))
+                            FireTrigger(trigger.To, gcta);
+                        break;
+                }
+        }
 
-                case AtomGraphChangeTypeEnum.EdgeRemoved:
-                    if (GraphUtil.ExistQueryOut(trigger.To, "ChangeTypeFilter", "OutputEdgeRemoved"))
-                        FireTrigger(trigger.To, gcta);
-                    break;
-
-                case AtomGraphChangeTypeEnum.OutputEdgeDisposed:
-                    if (GraphUtil.ExistQueryOut(trigger.To, "ChangeTypeFilter", "OutputEdgeDisposed"))
-                        FireTrigger(trigger.To, gcta);
-                    break;
-            }
-    }
-}
 
         public static void HandleMetaEdge(GraphChangeTransactionAtom gcta)
         {
+            IVertex changedVeretx = gcta.ChangedVertex;
 
+            foreach (IEdge trigger in GraphUtil.GetQueryOut(changedVeretx, "$GraphChangeTrigger", null))
+                switch (gcta.Type)
+                {
+                    case AtomGraphChangeTypeEnum.EdgeAdded:
+                        if (GraphUtil.ExistQueryOut(trigger.To, "ChangeTypeFilter", "MetaEdgeAdded"))
+                            FireTrigger(trigger.To, gcta);
+                        break;
+
+                    case AtomGraphChangeTypeEnum.EdgeRemoved:
+                        if (GraphUtil.ExistQueryOut(trigger.To, "ChangeTypeFilter", "MetaEdgeRemoved"))
+                            FireTrigger(trigger.To, gcta);
+                        break;
+                }
         }
     }
 }
