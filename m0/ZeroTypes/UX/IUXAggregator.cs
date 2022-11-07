59eb155ab812831114d7dbb646d6471f22aded29
rad9k <tereszczuk@gmail.com>
2022-11-07 22:28:30 +0100

    a

--- m0/ZeroTypes/UX/IUXAggregator.cs
+++ m0/ZeroTypes/UX/IUXAggregator.cs
@@ -8,73 +8,17 @@ using System.Threading.Tasks;
 
 namespace m0.ZeroTypes.UX
 {
-    public class UXAggregator: UXItem
+    public interface IUXAggregator: IUXItem
     {
-        static IVertex IsExpanded_meta = MinusZero.Instance.root.Get(false, @"System\Meta\ZeroTypes\UX\UXAggregator\IsExpanded");
-        static IVertex ExpandedSize_meta = MinusZero.Instance.root.Get(false, @"System\Meta\ZeroTypes\UX\UXAggregator\ExpandedSize");
-        static IVertex CollapsedSize_meta = MinusZero.Instance.root.Get(false, @"System\Meta\ZeroTypes\UX\UXAggregator\CollapsedSize");
+        bool IsExpanded { get; set; }
+        
+        UX.Size ExpandedSize { get; }
 
-        static IVertex Size_type = MinusZero.Instance.root.Get(false, @"System\Meta\ZeroTypes\UX\Size");
+        UX.Size ExpandedSizeCreate();
 
-        public UXAggregator(IEdge edge) : base(edge) { }
+        UX.Size CollapsedSize { get; }
 
-        public bool IsExpanded
-        {
-            get
-            {
-                IVertex val = GraphUtil.GetQueryOutFirst(Vertex, "IsExpanded", null);
-
-                if (val == null)
-                    return false;
-
-                return GraphUtil.GetBooleanValueOrFalse(val);
-            }
-            set
-            {
-                IVertex val = GraphUtil.GetQueryOutFirst(Vertex, "IsExpanded", null);
-
-                if (val == null)
-                    val = Vertex.AddVertex(IsExpanded_meta, value);
-                else
-                    val.Value = value;
-            }
-        }
-
-        public UX.Size ExpandedSize
-        {
-            get
-            {
-                IEdge val = GraphUtil.GetQueryOutFirstEdge(Vertex, "ExpandedSize", null);
-
-                if (val == null)
-                    return null;
-
-                return (UX.Size)TypedEdge.Get(val, typeof(UX.Size));
-            }
-        }
-
-        public UX.Size ExpandedSizeCreate()
-        {
-            return new UX.Size(VertexOperations.AddInstanceAndReturnEdge(Vertex, Size_type, ExpandedSize_meta));
-        }
-
-        public UX.Size CollapsedSize
-        {
-            get
-            {
-                IEdge val = GraphUtil.GetQueryOutFirstEdge(Vertex, "CollapsedSize", null);
-
-                if (val == null)
-                    return null;
-
-                return (UX.Size)TypedEdge.Get(val, typeof(UX.Size));
-            }
-        }
-
-        public UX.Size CollapsedSizeCreate()
-        {
-            return new UX.Size(VertexOperations.AddInstanceAndReturnEdge(Vertex, Size_type, CollapsedSize_meta));
-        }
+        UX.Size CollapsedSizeCreate();
     }
 
 }
