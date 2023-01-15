871f972ee0a950e76a6fbb97c2350c52bc38e024
rad9k <tereszczuk@gmail.com>
2023-01-15 14:43:03 +0100

    a

--- m0/ZeroTypes/UX/RectangleItem.xaml.cs
+++ m0/ZeroTypes/UX/RectangleItem.xaml.cs
@@ -35,15 +35,14 @@ namespace m0.ZeroTypes.UX
         //  public RectangleItem(IVertex baseEdgeVertex, IVertex parentVisualiser) : base(baseEdgeVertex)  {
         //    InitializeComponent();
         //}
-
-        /*
+/*
+    
         public override void VertexSetedUp()
         {
+            if (VisualiserClass != null)
             if (Vertex.Get(false, "VisualiserClass:") != null)
             {
-                ContentVisualiser = PlatformClass.CreatePlatformObject(Vertex.Get(false, "VisualiserClass:"), Vertex.Get(false, "BaseEdge:"));
-
-                //GraphUtil.ReplaceEdge(ContentVisualiser.Vertex, "BaseEdge", Vertex.Get(false, "BaseEdge:"));
+                ContentVisualiser = PlatformClass.CreatePlatformObject(VisualiserClass, BaseEdge);                
 
                 Grid.SetRow((UIElement)ContentVisualiser, 2);
 
@@ -55,14 +54,15 @@ namespace m0.ZeroTypes.UX
                 ContentVisualiser = null;
             }
 
-            //VisualiserUpdate(); done in base.VertexSetedUp();
            
-            if (Vertex.Get(false, "VisualiserVertex:") != null && ContentVisualiser != null)
-                Diagram.AddEdgesFromDefintion(ContentVisualiser.Vertex, Vertex.Get(false, "VisualiserVertex:"));
+           
+            if (VisualiserVertex != null && ContentVisualiser != null)
+                Diagram.AddEdgesFromDefintion(ContentVisualiser.Vertex, VisualiserVertex);
 
             base.VertexSetedUp();
         }
-
+        */
+        /*
         public override void VisualiserUpdate()
         {
             base.VisualiserUpdate();
@@ -219,6 +219,105 @@ namespace m0.ZeroTypes.UX
 
             //return base.VertexChange(exe);
         }
-    */
+        */
+        // UNDER        
+
+        static IVertex ShowMeta_meta = MinusZero.Instance.root.Get(false, @"System\Meta\ZeroTypes\UX\RectangleItem\ShowMeta");
+        static IVertex RoundEdgeSize_meta = MinusZero.Instance.root.Get(false, @"System\Meta\ZeroTypes\UX\RectangleItem\RoundEdgeSize");
+        static IVertex VisualiserClass_meta = MinusZero.Instance.root.Get(false, @"System\Meta\ZeroTypes\UX\RectangleItem\VisualiserClass");
+        static IVertex VisualiserVertex_meta = MinusZero.Instance.root.Get(false, @"System\Meta\ZeroTypes\UX\RectangleItem\VisualiserVertex");
+
+        public RectangleItem(IEdge edge) : base(edge) { }
+
+        public bool ShowMeta
+        {
+            get
+            {
+                IVertex val = GraphUtil.GetQueryOutFirst(Vertex, "ShowMeta", null);
+
+                if (val == null)
+                    return false;
+
+                return GraphUtil.GetBooleanValueOrFalse(val);
+            }
+            set
+            {
+                IVertex val = GraphUtil.GetQueryOutFirst(Vertex, "ShowMeta", null);
+
+                if (val == null)
+                    val = Vertex.AddVertex(ShowMeta_meta, value);
+                else
+                    val.Value = value;
+            }
+        }
+
+        public double RoundEdgeSize
+        {
+            get
+            {
+                IVertex val = GraphUtil.GetQueryOutFirst(Vertex, "RoundEdgeSize", null);
+
+                if (val == null)
+                    return 0;
+
+                return GraphUtil.GetDoubleValueOr0(val);
+            }
+            set
+            {
+                IVertex val = GraphUtil.GetQueryOutFirst(Vertex, "RoundEdgeSize", null);
+
+                if (val == null)
+                    val = Vertex.AddVertex(RoundEdgeSize_meta, value);
+                else
+                    val.Value = value;
+            }
+        }
+
+        public IVertex VisualiserClass
+        {
+            get
+            {
+                IVertex val = GraphUtil.GetQueryOutFirst(Vertex, "VisualiserClass", null);
+
+                if (val == null)
+                    return null;
+
+                return val;
+            }
+            set
+            {
+                IVertex val = GraphUtil.GetQueryOutFirst(Vertex, "VisualiserClass", null);
+
+                if (val == null)
+                    val = Vertex.AddVertex(VisualiserClass_meta, value);
+                else
+                    val.Value = value;
+            }
+        }
+
+        public IVertex VisualiserVertex
+        {
+            get
+            {
+                IVertex val = GraphUtil.GetQueryOutFirst(Vertex, "VisualiserVertex", null);
+
+                if (val == null)
+                    return null;
+
+                return val;
+            }
+            set
+            {
+                IVertex val = GraphUtil.GetQueryOutFirst(Vertex, "VisualiserVertex", null);
+
+                if (val == null)
+                    val = Vertex.AddVertex(VisualiserVertex_meta, value);
+                else
+                    val.Value = value;
+            }
+        }
+
+
+
     }
 }
