2eebfd9606c5d05abd514cd33d6316de3ba4c137
rad9k <tereszczuk@gmail.com>
2023-11-17 00:08:59 +0100

    a

--- m0/UIWpf/PlatformClassSimpleWrapper.xaml.cs
+++ m0/UIWpf/PlatformClassSimpleWrapper.xaml.cs
@@ -201,7 +201,7 @@ namespace m0.UIWpf
 
             if (currentSelectedEdgesFirst != null && 
                 GraphUtil.GetBooleanValueOrFalse(platformClassObject_IVisualiser.Vertex.Get(false, "ShowSelectedEdgesBaseEdge:")))
-                currentSelectedEdgesFirst = currentSelectedEdgesFirst.Get(false, @"To:/BaseEdge:");
+                currentSelectedEdgesFirst = currentSelectedEdgesFirst.Get(false, @"To:\BaseEdge:");
 
             ////////////////////////////////////////
             Interaction.BeginInteractionWithGraph();
