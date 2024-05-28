92729fe599a6860291f2b2f22a384314b403dbee
Radoslaw Tereszczuk <tereszczuk@gmail.com>
2024-05-28 17:23:49 +0200

    a

--- m0/UIWpf/Visualisers/CodeVisualiser.cs
+++ m0/UIWpf/Visualisers/CodeVisualiser.cs
@@ -71,7 +71,7 @@ namespace m0.UIWpf.Visualisers
                 new List<string> { @"", @"BaseEdge:\To:" },
                 "AtomVisualiserFull",
                 baseEdgeVertex,
-                UpdateBaseEdgeCallSchemeEnum.OmmitSecond);
+                UpdateBaseEdgeCallSchemeEnum.OmmitFirst);
 
             CodeControl = new CodeControl(Vertex, false, false, true);
 
