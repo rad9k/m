60ec4c5f53ffaa499882beec343565ded9a1155a
rad9k <tereszczuk@gmail.com>
2024-07-25 00:54:29 +0200

    a

--- m0/ZeroTypes/UX/CodeItem.xaml.cs
+++ m0/ZeroTypes/UX/CodeItem.xaml.cs
@@ -66,6 +66,12 @@ namespace m0.ZeroTypes.UX
 
             codeControl = new CodeControl(Vertex, true);
             codeControl.BaseEdgeToUpdated();
+
+            if (double.IsNaN(Width) && OwningVisualiser != null)
+            {
+                UpdateLayout();
+                UpdateDiagramLines();
+            }
         }
 
         public override void ViewAttributesUpdated()
