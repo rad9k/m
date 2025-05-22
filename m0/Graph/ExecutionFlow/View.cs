966aee1b2552e41f1b9f62beedda84cf487848e5
rad9k <tereszczuk@gmail.com>
2025-05-22 22:22:24 +0200

    a

--- m0/Graph/ExecutionFlow/View.cs
+++ m0/Graph/ExecutionFlow/View.cs
@@ -23,21 +23,89 @@ namespace m0.Graph.ExecutionFlow
             return exe.Stack;
         }
 
-        private static void ProcessCreateViewEvent(IEdge e)
+        private static void ProcessCreateViewEvent(IEdge eventEdge)
         {
-            IVertex evt, edge, edgeFrom, edgeTo, view;
+            IVertex eventVertex, edge, edgeFrom, edgeMeta, edgeTo, trigger, createView;
 
-            evt = e.To;
+            eventVertex = eventEdge.To;
 
-            edge = GraphUtil.GetQueryOutFirst(evt, "Edge", null);
+            edge = GraphUtil.GetQueryOutFirst(eventVertex, "Edge", null);
 
             edgeFrom = GraphUtil.GetQueryOutFirst(edge, "From", null);
 
-            edgeFrom = GraphUtil.GetQueryOutFirst(edge, "To", null);
+            edgeMeta = GraphUtil.GetQueryOutFirst(edge, "Meta", null);
 
-            view = GraphUtil.GetQueryOutFirst(evt, "Trigger", null);
+            edgeTo = GraphUtil.GetQueryOutFirst(edge, "To", null);
 
+            trigger = GraphUtil.GetQueryOutFirst(eventVertex, "Trigger", null);
 
+            createView = GraphUtil.GetQueryOutFirst(edgeMeta, "CreateView", null);
+
+            //
+
+            IVertex innerVertex = GraphUtil.GetQueryOutFirst(createView, "ViewInner", null);
+
+            if (innerVertex == null)
+                return;
+
+            IList<string> FromTriggerQueries = new List<string>();
+            IList<IVertex> FromToTransformFunctions = new List<IVertex>();
+            IList<string> ToTriggerQueries = new List<string>();
+            IList<IVertex> ToFromTransformFunctions = new List<IVertex>();
+
+            foreach (IEdge e in innerVertex)
+            {
+                if (GraphUtil.GetStringValue(e.Meta) != "Expression")
+                    continue;
+
+                IVertex expressionIs = GraphUtil.GetQueryOutFirst(e.To, "$Is", null);
+
+                if (expressionIs == null)
+                    continue;
+
+                switch (GraphUtil.GetStringValue(expressionIs))
+                {
+                    case "FromTriggerQuery":
+                        IVertex query = GraphUtil.GetQueryOutFirst(e.To, "Query", null);
+
+                        if (query == null)
+                            continue;
+
+                        FromTriggerQueries.Add(GraphUtil.GetStringValue(query));
+                        break;
+
+                    case "FromToTransformFunction":
+                        IVertex target = GraphUtil.GetQueryOutFirst(e.To, "Target", null);
+
+                        if (target == null)
+                            continue;
+
+                        FromToTransformFunctions.Add(target);
+                        break;
+
+                    case "ToTriggerQuery":
+                        IVertex query2 = GraphUtil.GetQueryOutFirst(e.To, "Query", null);
+
+                        if (query2 == null)
+                            continue;
+
+                        ToTriggerQueries.Add(GraphUtil.GetStringValue(query2));
+                        break;
+
+                    case "ToFromTransformFunction":
+                        IVertex target2 = GraphUtil.GetQueryOutFirst(e.To, "Target", null);
+
+                        if (target2 == null)
+                            continue;
+
+                        ToFromTransformFunctions.Add(target2);
+                        break;
+
+                }
+
+                int x = 0;
+
+            }
         }
     }
 }
