9e1991c6191c51c35fb8422681714656ec768889
Radosław Tereszczuk GL <ext.radoslaw.tereszczuk@globallogic.com>
2025-07-24 23:33:30 +0200

    d

--- run/m0/Lib/Net/Net.cs
+++ run/m0/Lib/Net/Net.cs
@@ -1,4 +1,7 @@
 ﻿using m0.Foundation;
+using m0.Graph;
+using m0.Network.Server;
+using Microsoft.AspNetCore.Server.Kestrel.Transport.NamedPipes;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +12,23 @@ namespace m0.Lib.Net
 {
     public class Net
     {
+        static IDictionary<IVertex, HttpServer> ServerInstances = new Dictionary<IVertex, HttpServer>();
+
         public static INoInEdgeInOutVertexVertex HttpServer_Start(IExecution exe)
         {
-            return exe.Stack;
+            IVertex thisVertex = GraphUtil.GetQueryOutFirst(exe.Stack, "this", null);
+
+            if (thisVertex == null)
+                return exe.Stack;
+
+            HttpServer server = null;
+
+            if (ServerInstances.ContainsKey(thisVertex))
+                server = ServerInstances[thisVertex];
+            else
+                server = CreateNamedPipeServerStreamContext()
+
+                return exe.Stack;
         }
 
         public static INoInEdgeInOutVertexVertex HttpServer_Stop(IExecution exe)
