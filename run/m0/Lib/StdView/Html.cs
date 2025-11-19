03520ed0be357e052bc8a811375a0cf3a4dc4a52
Radoslaw Tereszczuk <tereszczuk@gmail.com>
2025-11-19 16:39:53 +0100

    a

--- run/m0/Lib/StdView/Html.cs
+++ run/m0/Lib/StdView/Html.cs
@@ -11,6 +11,8 @@ namespace m0.Lib.StdView
 {
     public class Html
     {
+        static IVertex FormalTextLanguages_Vertex = MinusZero.Instance.Root.Get(false, @"System\FormalTextLangueges");
+
         public static INoInEdgeInOutVertexVertex AddColorsToCode(IExecution exe)
         {
             INoInEdgeInOutVertexVertex stack = exe.Stack;
@@ -21,11 +23,51 @@ namespace m0.Lib.StdView
             string FormalTextLanguage = GraphUtil.GetStringValue(FormalTextLanguage_Vertex);
             string text = GraphUtil.GetStringValue(text_Vertex);
 
+            IVertex ftl = GraphUtil.GetQueryOutFirst(FormalTextLanguages_Vertex, FormalTextLanguage, null);
+
             INoInEdgeInOutVertexVertex newStack = InstructionHelpers.CreateStack();
 
-            newStack.AddVertex(null, "kotek" + text);
+            if (ftl == null)
+                newStack.AddVertex(null, text);
+            else
+                newStack.AddVertex(null, AddColorsToCode_Process(ftl, text));
 
             return newStack;
+        }
+
+        private static string AddColorsToCode_Process(IVertex ftl, string text)
+        {
+            FormalTextLanguageDictinaries dict = DictionariesForFormalTextLanguageFactory.Get(ftl);
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char c = text[i];
+
+                if (dict.viewTokensDictionary.ContainsKey(c))
+                {
+                    List<ViewToken> vtl = dict.viewTokensDictionary[c];
+
+                    string match = ViewTokenMatch(vtl, text, i);
+
+                    if (match == null)
+                        sb.Append(c);
+                    else
+                    {
+
+                    }
+                }
+                else
+                    sb.Append(c);
+            }
+
+            return sb.ToString();
+        }
+
+        private static string ViewTokenMatch(List<ViewToken> vtl, string text, int i)
+        {
+            throw new NotImplementedException();
         }
     }
 }
