e3ea78c702df3daf650f8e5470740ff6bb3c756a
Radoslaw Tereszczuk <tereszczuk@gmail.com>
2026-02-10 16:48:20 +0100

    d

--- run/m0/ZeroUML/Instructions/BaseInstructions.cs
+++ run/m0/ZeroUML/Instructions/BaseInstructions.cs
@@ -2097,6 +2097,25 @@ namespace m0.ZeroUML.Instructions
             return newStack;
         }
 
+        public static INoInEdgeInOutVertexVertex DeepCopySet(ZeroCodeExecution exe, IVertex inputStack, IVertex instructionVertex, out bool isStackFrameReturn)
+        {
+            isStackFrameReturn = false;
+
+            IVertex expression = GetExpression(instructionVertex);
+
+            if (expression == null)
+                return exe.Stack;
+
+            INoInEdgeInOutVertexVertex expressionResult = exe.ExecuteInstructionByMontevideoPrinciples(inputStack, expression);
+
+            INoInEdgeInOutVertexVertex newStack = CreateStack();
+
+            foreach (IEdge e in expressionResult)
+                CopyVertex(e, newStack);
+
+            return newStack;
+        }
+
         // old, stackForNextExpression based version is in int the DoubleSemicolonOperator below
         public static INoInEdgeInOutVertexVertex DoubleColonOperator(ZeroCodeExecution exe, IVertex inputStack, IVertex instructionVertex, out bool isStackFrameReturn)
         {
