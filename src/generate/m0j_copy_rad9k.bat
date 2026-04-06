b2091cbe16011a0574115de7f41a52488a849e4e
rad9k <tereszczuk@gmail.com>
2026-04-06 23:09:34 +0200

    a

--- src/generate/m0j_copy_rad9k.bat
+++ src/generate/m0j_copy_rad9k.bat
@@ -1,2 +1,2 @@
-copy c:\Users\rad9k\Source\Repos\m\generate\m0_SYSTEM_GENERATE\bin\Debug\*.m0j c:\Users\rad9k\Source\Repos\m\run\m0_desktop\bin\Debug\net10.0-windows7.0\
-copy c:\Users\rad9k\Source\Repos\m\generate\m0_SYSTEM_GENERATE\bin\Debug\*.m0j c:\Users\rad9k\Source\Repos\m\run\m0_console\bin\Debug\net10.0\
+copy c:\Users\rad9k\Source\Repos\m\src\generate\m0_SYSTEM_GENERATE\bin\Debug\*.m0j c:\Users\rad9k\Source\Repos\m\src\run\m0_desktop\bin\Debug\net10.0-windows7.0\
+copy c:\Users\rad9k\Source\Repos\m\src\generate\m0_SYSTEM_GENERATE\bin\Debug\*.m0j c:\Users\rad9k\Source\Repos\m\src\run\m0_console\bin\Debug\net10.0\
