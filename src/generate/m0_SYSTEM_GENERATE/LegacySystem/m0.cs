68f58caea02efec9c5b3c175f3465dccff402a85
rad9k <tereszczuk@gmail.com>
2026-06-24 00:47:12 +0200

    d

--- src/generate/m0_SYSTEM_GENERATE/LegacySystem/m0.cs
+++ src/generate/m0_SYSTEM_GENERATE/LegacySystem/m0.cs
@@ -6927,10 +6927,7 @@ namespace m0
 
             IVertex inherits = Root.Get(false, @"System\Meta\Base\Vertex\$Inherits");
 
-            package.AddEdge(inherits, Root.Get(false, @"System\Meta\ZeroUML\Types\Component"));
-
-            zu.Get(false, @"Types\Interface").AddEdge(inherits, package);
-        
+            package.AddEdge(inherits, Root.Get(false, @"System\Meta\ZeroUML\Types\Component"));                    
         }
 
         void CreateSystemMetaCustomDomain()
