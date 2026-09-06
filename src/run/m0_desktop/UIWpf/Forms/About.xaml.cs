e0a7701731f36306f3f3d7b31930f6cb364a7ed2
rad9k <tereszczuk@gmail.com>
2026-09-06 22:22:28 +0200

    1.0

--- src/run/m0_desktop/UIWpf/Forms/About.xaml.cs
+++ src/run/m0_desktop/UIWpf/Forms/About.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace m0.UIWpf.Forms
@@ -25,6 +27,15 @@ namespace m0.UIWpf.Forms
             InitializeComponent();
 
             ShowDialog();
+        }
+
+        private void WebsiteHyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri)
+            {
+                UseShellExecute = true
+            });
+            e.Handled = true;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
